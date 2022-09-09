b3c1db75fd5508ae255fab72e4810f5e7c2c8d6e
mwaniki-david <dmwaniki353@gmail.com>
2022-09-09 04:09:27 -0700

    5-printf.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+
+/**
+* main - print the string in the put function
+*
+*description: using the main function
+*this program prints "Programming is like building a multilingual puzzle
+*Return: 0
+*/
+int main(void)
+{
+pu
+
+
