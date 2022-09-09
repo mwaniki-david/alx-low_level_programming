38eb593100d1dd91234d84563437b125cfafa5b2
mwaniki-david <dmwaniki353@gmail.com>
2022-09-09 05:02:23 -0700

    4-puts.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
-
+#include <stdio.h>
+/**
+ * main -print the string in the put function
+ *
+ * Description:using the main function
+ * this program prints  programing is like building a multilingual puzzle
+ * Return: 0
+ */
+int main(void)
+{      printf("with proper grammar, but the outcomr is a piece of art,\n");
+	return (0);
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print the string in the put function
+ *
+ * Description:use the main function
+ * this program prints "programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+int main(void)
+{	char c;
+	int i;
+	long 1i;
+	long long 11i;
+	float f;
+
+	printf("size of a char: %1d byte(s)\n", sizeof(c));
+	printf("size of an int: %1d byte(s)\n", sizeof(i));
+	printf("size of a long int: %1d byte(s)\n", sizeof(1i));
+	printf("size of a long long int: %1d byte(s)\n", sizeof(11i));
+	printf("size of a float: %1d byte(s)\n", sizeof(f));
+	return (0)
+}
+
