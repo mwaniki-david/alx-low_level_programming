ef4aa65b48f4465ffb7aab4e10526b671fce92d9
mwaniki-david <dmwaniki353@gmail.com>
2022-09-12 07:15:07 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,7 @@
  * Return: 0
  */
 int main(void)
-{      printf("with proper grammar, but the outcomr is a piece of art,\n");
+{      printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
 
