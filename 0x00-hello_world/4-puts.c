b253c005957c6e0cda7e4a8cff39a2cf22858256
mwaniki-david <dmwaniki353@gmail.com>
2022-09-12 07:10:44 -0700

    -Wimplicit-function-declaration

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
-#include <studio.h>
-
+#include <stdio.h>
 /**
-* main - print the string in the put function
-*
-*description: using the main function
-*this program prints "Programming is like building a multilingual puzzle
-*Return: 0
-*/
-int main(void)
+ * main-print the stringin the put function
+ *
+ * Description:using the main function
+ * this program prints "programming is like building a multilingual puzzle
+ * Return(0);
+ */
+int main (void)
 {
-pu
+	puts("\"programming is like building a multilingual puzzle");
+	Return(0);
+}
 
 
