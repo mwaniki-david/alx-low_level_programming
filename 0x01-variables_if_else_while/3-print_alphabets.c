4768a7eca5f361b9783f2b249224788b11f61d59
mwaniki-david <dmwaniki353@gmail.com>
2022-09-12 13:54:47 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,8 @@
+#include <stdio>
+/**
+*main -print if  the number is positive ,zero or negative
+*
+*description:using the main function
+*this program prints "programming is positive, zero or negative
+*Return: 0
+*/
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print if the number is potive,zero or negative
+ *
+ * Description:using the main function
+ * this program prints "programming is positive,zero or negative "
+ * return:0
+ */
+int main(void)
+{
+	char ch;
+	for (ch= 'a' ; ch<= 'z' ;ch++)
+	{
+		if (ch != 'e' && ch != 'q'
+				{
+				putchar(ch);
+				}
+				}
+				putchar('\n');
+				return(0);
+				
+				}
