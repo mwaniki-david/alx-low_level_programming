7709456ef2cc591df631681f510f8c8272388e24
mwaniki-david <dmwaniki353@gmail.com>
2022-09-14 04:35:02 -0700

    pp

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,18 +3,12 @@
 
 
 /**
- *
- *  * add - Adds two integers.
- *
- *   * @num1: The first integer to be added.
- *
- *    * @num2: The second integer to be added.
- *
- *     *
- *
- *      * Return: The result of the addition.
- *
- *       */
+ *  add - Adds two integers.
+ *  @num1: The first integer to be added.
+ *  @num2: The second integer to be added.
+ *  
+ *  Return: The result of the addition.
+ */
 
 int add(int num1, int num2)
 
