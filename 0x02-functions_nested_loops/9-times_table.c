f7ac914dcb2e0ce5cf221a9dac9bcfe5be5ce84e
mwaniki-david <dmwaniki353@gmail.com>
2022-09-14 03:39:51 -0700

    critical

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,103 +1,58 @@
-Task 9.txt
-
-Who has access
-
-A
-
-System properties
-
-Type
-
-Text
-
-Size
-
-455 bytes
-
-Storage used
-
-455 bytes
-
-Location
-
-0x02. C - Functions, nested loops
-
-Owner
-
-Antony Bahati
-
-Modified
-
-13 Sept 2022 by Antony Bahati
-
-Opened
-
-13:13 by me
-
-Created
-
-13 Sept 2022
-
-No description
-
-Viewers can download
-
 #include "main.h"
 
 
 
 /**
-
- *times_table - Prints the 9 times table, starting with 0.
-
- */
-
+ *
+ *  *times_table - Prints the 9 times table, starting with 0.
+ *
+ *   */
 void times_table(void)
 
 {
 
-	int num, mult, prod;
+		int num, mult, prod;
 
 
 
-	for (num = 0; num <= 9; num++)
+			for (num = 0; num <= 9; num++)
 
-	{
+					{
 
-		_putchar('0');
-
-
-
-		for (mult = 1; mult <= 9; mult++)
-
-		{
-
-			_putchar(',');
-
-			_putchar(' ');
+								_putchar('0');
 
 
 
-			prod = num * mult;
+										for (mult = 1; mult <= 9; mult++)
+
+													{
+
+																	_putchar(',');
+
+																				_putchar(' ');
 
 
 
-			if (prod <= 9)
-
-				_putchar(' ');
-
-			else
-
-				_putchar((prod / 10) + '0');
+																							prod = num * mult;
 
 
 
-			_putchar((prod % 10) + '0');
+																										if (prod <= 9)
 
-		}
+																															_putchar(' ');
 
-		_putchar('\n');
+																													else
 
-	}
+																																		_putchar((prod / 10) + '0');
+
+
+
+																																_putchar((prod % 10) + '0');
+
+																																		}
+
+												_putchar('\n');
+
+													}
 
 }
