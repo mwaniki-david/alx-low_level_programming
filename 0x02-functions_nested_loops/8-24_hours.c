737082f8f0cdc6e56df87e63128cc844b2d6e626
mwaniki-david <dmwaniki353@gmail.com>
2022-09-14 04:24:37 -0700

    p

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,11 +13,10 @@ void jack_bauer(void)
 
 	while (hours <= 23)
 	{
-        while (minutes <= 59)
+		while (minutes <= 59)
 
 	{
-
-        mins_remainder = minutes % 10;
+       	mins_remainder = minutes % 10;
 	hours_remainder = hours % 10;
 	_putchar(hours / 10 + '0');
 	_putchar(hours_remainder + '0');
