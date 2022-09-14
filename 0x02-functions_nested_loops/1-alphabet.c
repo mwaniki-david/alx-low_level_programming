46d8abab9b91ff2bfd1cfa735df649f50655eb89
mwaniki-david <dmwaniki353@gmail.com>
2022-09-14 03:22:29 -0700

    changes

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,11 +2,8 @@
 
 /**
  * print_alphabet - function to print abc
-
  *
-
  * Return: 0
-
  */
 
 void print_alphabet(void)
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,10 @@
 #include "main.h"
 
 /**
-
  * _islower - function to check for lowercase character
-
  * @c:  is the int that will use for the argument of the function
-
  * Return: 0
-
  */
-
 int _islower(int c)
 
 {
