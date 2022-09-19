131b4c569d699ee11e429142859c4c6898c757f7
mwaniki-david <dmwaniki353@gmail.com>
2022-09-19 04:09:22 -0700

    no 1-c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,19 @@
-dylan the fucking king
+#include "main.h"
+/**
+ * _pusts-prints a string
+ * @str: string to print
+ *
+ * Description: print a screen
+ * on success:return no error
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		putchar (*(str + i));
+		i++;
+	}
+	putchar (10);
+}
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,17 @@
-kjnnunnunununll klnlnnln
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * print_rev-print a string in reverse order
+ * @s: string to reverse
+ * return: nothing
+ */
+void print_rev(char *s)
+{
+	int len = strlen(s);
+
+	while (len--)
+		putchar(*(s + len));
+	putchar (10);
+}
+
