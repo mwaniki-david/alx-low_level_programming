d81cbc02f04d812079b5a222a4d3c16bc848727b
mwaniki-david <dmwaniki353@gmail.com>
2022-09-19 15:13:15 -0700

    commitments

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -26,6 +26,7 @@ void rev_string(char *s)
 int _strlen(char *s)
 {
 	int len = 0;
+
 	while (*(s + len) != '\0')
 		len++;
 	return (len);
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * puts2 -print every other character
+ * puts2 - prints every other character
  * @str: string
  *
- * return: nothing
+ * Return: nothing
  */
 void puts2(char *str)
 {
 	int i = 0;
 
-	while (*(str + i) != '\n')
+	while (*(str + i) != '\0')
 	{
-		if (i % 2 ==0)
+		if (i % 2 == 0)
 			putchar(*(str + i));
 		i++;
 	}
 	putchar(10);
 }
-
-
