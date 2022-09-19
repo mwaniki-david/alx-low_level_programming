bf19470664b4bc47378180d9e839c4bf20176d85
mwaniki-david <dmwaniki353@gmail.com>
2022-09-19 03:06:53 -0700

    no 0-i

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,15 @@
-nnnooioij
+#include "main.h"
+/**
+ * swap -int
+ * @a: first integer
+ * @b: second integer
+ * Return:returns nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+
+	*a = *b;
+	*b = tmp;
+}
+
