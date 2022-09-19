69ca90deec8c0c246a7efeb9d7753745ac18de76
mwaniki-david <dmwaniki353@gmail.com>
2022-09-19 03:48:51 -0700

    n0 1-c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,2 +1,14 @@
+#include "main.h"
+/**
+ * _strlen-returns the strength of a string
+ * @s: string
+ * Return: returns length as integer;
+ */
+int _strlen(char *s)
+{
+	int len = 0;
 
- 3 no2 
+	while(*(s + len) != '\0')
+		len++;
+	return (len);
+}
