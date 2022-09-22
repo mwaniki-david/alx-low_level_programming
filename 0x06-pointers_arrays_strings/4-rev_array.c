df80ecb2571e0b9f22a3eef9cf7758892c7d447c
mwaniki-david <dmwaniki353@gmail.com>
2022-09-22 13:50:15 -0700

    4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
- *  reverse_array - function that reverse content of array
- *  @a: array
- *  @n: number of elements
+ * reverse_array - function that reverse content of array
+ * @a: array
+ * @n: number of elements
  */
-
 void reverse_array(int *a, int n)
 {
-	int tmp, index;
+	int tmp ,index;
 	for (index = n - 1; index > n / 2; index--)
 	{
 		tmp = a[n - 1 - index];
