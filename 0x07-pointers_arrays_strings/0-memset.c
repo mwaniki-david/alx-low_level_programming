dd22261690165d598eb5c89dcba5bf9bb78c7761
mwaniki-david <dmwaniki353@gmail.com>
2022-09-27 00:19:21 -0700

    xxxx

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,39 +7,13 @@
  *
  *  Return: Nothing.
  */
-void simple_print_buffer(char *buffer, unsigned int size)
+char *_memset(char *s char b, unsigned int n)
 {
-	unsigned int i;
-
-	i = 0;
-	while (i< size)
+	while (n)
 	{
-		if (i % 10)
-		{
-			printf(" ");
-		}
-		if (!(i % 10) && i)
-		{
-			printf("\n");
-		}
-		printf("0x%02x", buffer[i]);
-		i++;
+		s[n - 1] =b;
+		n--;
 	}
-	 printf("\n");
-}
-/**
- * main -check the code 
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	char buffer[98] = {0x00};
-
-	simple_print_buffer(buffer, 98);
-	 _memset(buffer, 0x01, 95);
-	 printf("-------------------------------------------------\n");
-	  simple_print_buffer(buffer, 98);
-	   return (0);
+	return (s);
 }
 
