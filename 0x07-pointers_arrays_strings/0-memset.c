1f4d995e3ee66df5fa06bf2badc6c80a91346953
mwaniki-david <dmwaniki353@gmail.com>
2022-09-27 00:32:32 -0700

    why not

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,13 +7,16 @@
  *
  *  Return: Nothing.
  */
-char *_memset(char *s char b, unsigned int n)
+char *_strcat(char *dest, char *src)
 {
-	while (n)
-	{
-		s[n - 1] =b;
-		n--;
-	}
-	return (s);
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
 }
+
 
