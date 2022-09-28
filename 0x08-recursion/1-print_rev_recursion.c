8079b11f80764bb4457baf4a6be9fb5a99186a8a
mwaniki-david <dmwaniki353@gmail.com>
2022-09-28 15:17:54 -0700

    uyu

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * _print_rev_recursion - print a string in reverse
- * @s :string to be printed 
+ * @s:string to be printed 
  *
- * return :void
+ * return:void
  */
 void _print_rev_recursion(char *s)
 {
