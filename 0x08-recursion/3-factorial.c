1c8d5167591d94a291984ebdec066cc78180df6c
mwaniki-david <dmwaniki353@gmail.com>
2022-09-28 14:27:00 -0700

    dsds

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,16 +1,18 @@
 #include "main.h"
 /**
- * factorial -returns the factorial of a given number
- * @n :number to be used 
- *
+ * factorial - returns the factorial of a given number
+ * @n: number to be used
  * Return :the factorial of the number
  */
-int factorial(int n)
+in factorial(int n)
 {
 	int next_factorial;
 
 	if (n < 0)
 		return (-1);
-	next_factorial = factorial(n -1);
-		return(n * next_factorial);
+	else if (n == 0)
+		return (1);
+	next_factorial =factorial(n - 1);
+	return (n * next_factorial);
 }
+
