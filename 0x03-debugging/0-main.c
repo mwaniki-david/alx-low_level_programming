1de25458ea27cd1b0003e120ae76a3f54b346799
mwaniki-david <dmwaniki353@gmail.com>
2022-10-02 01:27:08 -0700

    lil

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,15 +3,15 @@
  * main-prints positive or negative integer
  * Return:0
  */
-int main (void)
+int main(void)
 {
-
 	int i;
 
-	i =0;
+	i = 0;
 
 	positive_or_negative(i);
 
-	return(0);
+	return (0);
 }
+
 
