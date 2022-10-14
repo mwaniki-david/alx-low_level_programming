065ca2eafe44b9529658364c8930df310f06b678
mwaniki-david <dmwaniki353@gmail.com>
2022-10-14 14:03:29 +0300

    say no to drugs

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,8 +9,7 @@
  *
  * Return: no return
  */
-void print_strings(const char *separator, const unsigned int n, ...)
-{
+void print_strings(const char *separator, const unsigned int n, ...);{
 	va_list valist;
 	unsigned int i;
 	char *str;
